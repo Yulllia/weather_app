fe95a45e1dc2a430f116c588805b1d8861c7127b
Yulllia <shyshka20@gmail.com>
2025-02-05 21:36:23 +0200

    changes to api

--- src/app/api/fetchWeather/route.ts
+++ src/app/api/fetchWeather/route.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
 
 
-export async function GET(req: { url: string | URL; }) {
-    const { searchParams } = new URL(req.url);
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
     const latitude = searchParams.get('latitude');
     const longitude = searchParams.get('longitude');
     const currentDate = searchParams.get('currentDate');
@@ -11,9 +12,9 @@ export async function GET(req: { url: string | URL; }) {
         const response = await axios.get(
             `${process.env.NEXT_PUBLIC_WEATHER_API_BASE}?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode&daily=temperature_2m_max,temperature_2m_min&start_date=${currentDate}&end_date=${currentDate}`
         );
-        return new Response(JSON.stringify(response.data), { status: 200 });
+        return new NextResponse(JSON.stringify(response.data), { status: 200 });
     } catch (error) {
         console.log(error);
-        return new Response("Error fetching weather data", { status: 500 });
+        return new NextResponse("Error fetching weather data", { status: 500 });
     }
 }
--- src/app/api/geocode/route.ts
+++ src/app/api/geocode/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const address = searchParams.get('address');
 

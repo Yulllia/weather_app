cd097dfe09f73e2cb469de8d89a017ba102ee02c
Yulllia <shyshka20@gmail.com>
2025-02-05 21:48:57 +0200

    add users api changes

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,16 +1,17 @@
 import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
 
 
-export async function GET(req: { url: string | URL; }) {
+export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
     const count = searchParams.get('results');
     const page = searchParams.get('page');
 
     try {
         const response = await axios.get(`${process.env.NEXT_PUBLIC_USERS_API}?results=${count}&page=${page}`);
-        return new Response(JSON.stringify(response.data.results), { status: 200 });
+        return new NextResponse(JSON.stringify(response.data.results), { status: 200 });
     } catch (error) {
         console.error('Error fetching users:', error);
-        return new Response('Error fetching users', { status: 500 });
+        return new NextResponse('Error fetching users', { status: 500 });
     }
 }
